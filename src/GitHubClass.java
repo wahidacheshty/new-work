b312e8f670edd4990b3795afe060310aa3147ade
wahida <cheshtywahida@gmail.com>
2022-05-10 15:40:30 -0400

    changes after break

--- src/GitHubClass.java
+++ src/GitHubClass.java
@@ -3,7 +3,15 @@ public class GitHubClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+    
+		int a =10;
+				int b = 10;
+		
+		int c = a+b;
+		
+		System.out.println(c);
+		
+		
 	}
 
 }
